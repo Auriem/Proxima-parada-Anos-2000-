1f365bc5cd7465020ab192beee53885d327e83b6
Auriem <auriemcompany@gmail.com>
2025-08-18 14:55:08 -0300

    Update send-ticket-manually.js

--- netlify/functions/send-ticket-manually/send-ticket-manually.js
+++ netlify/functions/send-ticket-manually/send-ticket-manually.js
@@ -1,6 +1,11 @@
+import { Resend } from 'resend';
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const handler = async (event) => {
   try {
